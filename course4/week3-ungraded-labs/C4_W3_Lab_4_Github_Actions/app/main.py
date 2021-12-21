8cb511a9bf9a3251a66931f79caf338ee8f8406a
Mikhail Kyraha <1049133+kyraha@users.noreply.github.com>
2021-12-21 00:02:33 -0600

    New classifier (95)

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel, conlist
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
-with open("models/wine.pkl", "rb") as file:
+with open("models/wine-95.pkl", "rb") as file:
     clf = pickle.load(file)
 
 
@@ -17,7 +17,6 @@ class Wine(BaseModel):
     batches: List[conlist(item_type=float, min_items=13, max_items=13)]
 
 
-# Some comment
 @app.post("/predict")
 def predict(wine: Wine):
     batches = wine.batches
